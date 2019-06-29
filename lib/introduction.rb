99d3a50ac0009994ab30b77b8c32bdad2c02f2d6
Austin Trow <austintrow@gmail.com>
2019-06-29 23:25:02 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,11 @@
 def introduction(name)
-  puts "Sup, #{name}?";
+  puts "Hi, my name is #{name}.";
+end
+
+def introduction_with_language(name, language)
+  "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+def introduction_with_language_optional(name, language="ruby")  
+  "Hi, my name is #{name} and I am learning to program in #{language}."
 end
